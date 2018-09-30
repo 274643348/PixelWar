a679b0e7fc36bd516f5b861b3414952e488f5330
XiaoYan <liujingyan@congmingpai.com>
2018-09-30 18:10:08 +0800

    eslint未完成测试

--- assets/Script/GameScripts/Enemy.ts
+++ assets/Script/GameScripts/Enemy.ts
@@ -1,54 +1,60 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Enemy extends cc.Component {
+  @property
+  speed: number = 10;
+  @property
+  isStop: boolean = false;
 
-    @property
-    speed: number = 10;
-    @property
-    isStop: boolean = false;
+  private direction = cc.v2(1, 0);
 
-    private direction= cc.v2(1,0);
+  onLoad() {
+    this.direction =
+      Math.floor(Math.random() * 2) === 0 ? cc.v2(1, 0) : cc.v2(1, 0);
+    this.speed = Math.floor(Math.random() * 10);
+    this.node.position = cc.v2(
+      Math.random() * 1080 - 540,
+      Math.random() * 1920 - 860
+    );
+  }
 
-
-    onLoad () {
-        this.direction = Math.floor((Math.random() * 2)) === 0 ? cc.v2(1,0):cc.v2(1,0);
-        this.speed = Math.floor((Math.random()*10));
-        this.node.position = cc.v2((Math.random()*1080 -540),(Math.random()*1920 - 860));
+  update() {
+    if (this.isStop) {
+      return;
     }
 
-    update () {
-        if(this.isStop)
-        {
-            return;
-        }
-
-        let move = cc.v2(this.direction.x * this.speed,this.direction.y * this.speed );
-        this.node.setPosition(cc.v2(this.node.getPosition()).add(move));
-        
-        if (this.node.getPositionX() >1080 /2) {
-            this.node.x = -1080 /2;
-        }
-        if (this.node.getPositionX() < -1080 /2) {
-            this.node.x = 1080 /2;
-        }
-        if (this.node.getPositionY() >1920 /2) {
-            this.node.y = -1920 /2;
-        }
-        if (this.node.getPositionY() < -1920 /2) {
-            this.node.y = 1920 /2;
-        }
+    let a = [2, 3, 4, 5];
+    for (let index = 0; index < a.length; index++) {
+      console.log("a");
     }
+    let move = cc.v2(
+      this.direction.x * this.speed,
+      this.direction.y * this.speed
+    );
+    this.node.setPosition(cc.v2(this.node.getPosition()).add(move));
 
-    public setDirection(dir:cc.Vec2)
-    {
-        dir.normalizeSelf();
-        // console.log(dir.x +"****"+dir.y);
-
+    if (this.node.getPositionX() > 1080 / 2) {
+      this.node.x = -1080 / 2;
     }
-
-    public die(){
-        this.isStop = true;
-        this.node.removeFromParent();
+    if (this.node.getPositionX() < -1080 / 2) {
+      this.node.x = 1080 / 2;
     }
+    if (this.node.getPositionY() > 1920 / 2) {
+      this.node.y = -1920 / 2;
+    }
+    if (this.node.getPositionY() < -1920 / 2) {
+      this.node.y = 1920 / 2;
+    }
+  }
+
+  public setDirection(dir: cc.Vec2) {
+    dir.normalizeSelf();
+    // console.log(dir.x +"****"+dir.y);
+  }
+
+  public die() {
+    this.isStop = true;
+    this.node.removeFromParent();
+  }
 }
